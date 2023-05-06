fba1ac7522cc995659d63b172998ca722fa27637
AlexFZR <abadragan@gmail.com>
2023-05-06 13:37:16 +0200

    bytes & runes

--- Practical Go Foundations/Day 1/banner/banner.go
+++ Practical Go Foundations/Day 1/banner/banner.go
@@ -3,11 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	banner("Go", 6)
+	s := "G☺"
+	banner(s, 6)
+	for i, v := range s {
+		fmt.Println(i, "=", v)
+	}
+
+	// byte (uint8)
+	// rune (int32)
+
+	b := s[0]
+	fmt.Printf("%c of type %T\n", b, b)
+	u := s[0]
+	fmt.Printf("%c of type %T\n", u, u)
 }
 
 func banner(text string, width int) {
 	padding := (width - len(text)) / 2
+	fmt.Println(len(text))
 	for i := 0; i < padding; i++ {
 		fmt.Print(" ")
 	}
