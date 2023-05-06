a33f047db72ee637c90c66cdc6a3fea963b6e800
AlexFZR <abadragan@gmail.com>
2023-05-06 17:31:01 +0200

    error handling

--- Practical Go Foundations/Day 1/sha1/sha1.go
+++ Practical Go Foundations/Day 1/sha1/sha1.go
@@ -1,6 +1,15 @@
 package main
 
-import "os"
+import (
+	"compress/gzip"
+	"crypto/sha1"
+	"io"
+	"os"
+)
+
+func main() {
+	sha1Sum("http.log.gz")
+}
 
 // $ cat http.log.gz| gunzip | sha1sum
 func sha1Sum(fileName string) (string, error) {
@@ -10,6 +19,17 @@ func sha1Sum(fileName string) (string, error) {
 		return "", nil
 	}
 	defer file.Close() //defered are called in LIFO order
+
+	r, err := gzip.NewReader(file)
+	if err != nil {
+		return "", err
+	}
+	//io.CopyN(os.Stdout, r, 100)
+	w := sha1.New()
+
+	if _, err := io.Copy(w, r); err != nil {
+		return "", err
+	}
 
 	return "", nil
 }
