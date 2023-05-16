5dfcd57faa8464c5b4f2eb27979f245104c0722c
AlexFZR <abadragan@gmail.com>
2023-05-16 18:14:58 +0200

    go routine in loop

--- Practical Go Foundations/Day 3/go_chan/go_chan.go
+++ Practical Go Foundations/Day 3/go_chan/go_chan.go
@@ -9,5 +9,12 @@ func main() {
 	go fmt.Println("goroutine")
 	fmt.Println("main")
 
+	for i := 0; i < 3; i++ {
+		// BUG: All go routines use the "i" for the loop
+		go func() {
+			fmt.Println(i)
+		}()
+	}
+
 	time.Sleep(10 * time.Millisecond)
 }
