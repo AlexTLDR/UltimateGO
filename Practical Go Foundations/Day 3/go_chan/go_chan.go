d65c4aac0e7b8173fefed8a447be5ea3524887f0
AlexFZR <abadragan@gmail.com>
2023-05-16 18:17:04 +0200

    fix1: use a parameter

--- Practical Go Foundations/Day 3/go_chan/go_chan.go
+++ Practical Go Foundations/Day 3/go_chan/go_chan.go
@@ -10,10 +10,16 @@ func main() {
 	fmt.Println("main")
 
 	for i := 0; i < 3; i++ {
-		// BUG: All go routines use the "i" for the loop
+		/* BUG: All go routines use the "i" for the loop
 		go func() {
 			fmt.Println(i)
 		}()
+		*/
+
+		//Fix 1: use a parameter
+		go func(n int) {
+			fmt.Println(n)
+		}(i)
 	}
 
 	time.Sleep(10 * time.Millisecond)
