194da9dcefdf6e3b746c863725507a724d077074
AlexFZR <abadragan@gmail.com>
2023-05-17 12:29:32 +0200

    go routines with WaitGroup

--- Practical Go Foundations/Day 3/sites_time/sites_time.go
+++ Practical Go Foundations/Day 3/sites_time/sites_time.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -20,4 +21,29 @@ func siteTime(url string) {
 	if _, err := io.Copy(io.Discard, resp.Body); err != nil {
 		log.Printf("ERROR: %s -> %s", url, err)
 	}
+
+	duration := time.Since(start)
+	log.Printf("INFO: %s -> %v", url, duration)
+}
+
+func main() {
+	//siteTime("https://google.com")
+	urls := []string{
+		"https://google.com",
+		"https://gsp.ro",
+		"https://amazon.de",
+		"https://fake-site.org",
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(urls))
+	for _, url := range urls {
+		// wg.Add(1)
+		url := url
+		go func() {
+			defer wg.Done()
+			siteTime(url)
+		}()
+	}
+	wg.Wait()
 }
