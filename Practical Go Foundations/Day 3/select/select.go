152ede01b3511741556133f9bb387a9f613edb5e
AlexFZR <abadragan@gmail.com>
2023-05-20 12:49:35 +0200

    printed the channels

--- Practical Go Foundations/Day 3/select/select.go
+++ Practical Go Foundations/Day 3/select/select.go
@@ -1,22 +1,25 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	ch1, ch2 := make(chan int), make(chan int)
-	myfunc := func(duration int) {
-		time.Sleep(time.Duration(duration) * time.Millisecond)
-		ch1 <- 1
-	}
+	// myfunc := func(duration int) {
+	// 	time.Sleep(time.Duration(duration) * time.Millisecond)
+	// 	ch1 <- 1
+	// }
 
-	func(duration int) {
-		time.Sleep(time.Duration(duration) * time.Millisecond)
-		ch1 <- 1
-	}(10)
+	// func(duration int) {
+	// 	time.Sleep(time.Duration(duration) * time.Millisecond)
+	// 	ch1 <- 1
+	// }(10)
 
-	go myfunc(10000)
+	// go myfunc(10000)
 
-	go myfunc(20000)
+	// go myfunc(20000)
 
 	go func() {
 		time.Sleep(10 * time.Millisecond)
@@ -24,7 +27,16 @@ func main() {
 	}()
 
 	go func() {
-		time.Sleep(20 * time.Millisecond)
+		time.Sleep(2 * time.Millisecond)
 		ch2 <- 2
 	}()
+
+	select {
+	case val := <-ch1:
+		fmt.Println("ch1", val)
+
+	case val := <-ch2:
+		fmt.Println("ch2", val)
+	}
+
 }
