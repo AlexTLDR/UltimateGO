4b8cac016fde4e54ae0ee99765573a333b018e9b
AlexFZR <abadragan@gmail.com>
2023-05-20 16:46:21 +0200

    switched to testify

--- Practical Go Foundations/Day 4/nlp/nlp_test.go
+++ Practical Go Foundations/Day 4/nlp/nlp_test.go
@@ -3,13 +3,18 @@ package nlp
 import (
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestTokenize(t *testing.T) {
 	test := "What's on second?"
 	expected := []string{"what", "s", "on", "second"}
 	tokens := Tokenize(text)
+	require.DeepEqual(t, expected, tokens)
+	/* Before testify
 	if !reflect.DeepEqual(expected, tokens) {
 		t.Fatalf("expected %#v, got %#v", expected, tokens)
 	}
+	*/
 }
